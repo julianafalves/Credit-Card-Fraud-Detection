91b589f0ed4db76dffeefb855a658c5521a8e5b3
julianafalves <julianaferreira@estudante.ufscar.br>
2024-06-08 16:44:03 -0300

    add prod folder/split train test

--- src/credit_card_fraud_detection/pipelines/data_engineering/nodes.py
+++ src/credit_card_fraud_detection/pipelines/data_engineering/nodes.py
@@ -1,7 +1,21 @@
-"""
-This is a boilerplate pipeline 'data_engineering'
-generated using Kedro 0.19.6
-"""
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def load(df):
     print(df.head())
+
+def split_data(df, params: dict):
+    """
+    Split DataFrame into training and testing sets.
+
+    Args:
+    df (DataFrame): Input DataFrame.
+    params (dict): Parameters for train-test split.
+
+    Returns:
+    tuple: X_train, X_test, y_train, y_test.
+    """
+    X_train, X_test, y_train, y_test = train_test_split(df.drop('Class',axis=1), df['Class'], 
+                                                        test_size=params['test_size'], 
+                                                        random_state=params['random_state'])
+    return X_train, X_test, y_train, y_test
--- src/credit_card_fraud_detection/pipelines/data_engineering/pipeline.py
+++ src/credit_card_fraud_detection/pipelines/data_engineering/pipeline.py
@@ -4,9 +4,15 @@ generated using Kedro 0.19.6
 """
 
 from kedro.pipeline import Pipeline, pipeline, node
-from .nodes import load
+from .nodes import load,split_data
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([node(func=load,
                           inputs='creditCard',
-                          outputs=None)])
+                          outputs=None),
+                    node(
+                        func=split_data,
+                        inputs=['creditCard','params:split'],
+                        outputs=[ 'X_train', 'X_test', 'y_train', 'y_test'],
+                        name="split_data",
+                    )])
