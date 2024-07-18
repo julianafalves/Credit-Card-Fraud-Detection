169a7bd05d6b3d58ba770ba6071534547b8f57ef
julianafalves <julianaferreira@estudante.ufscar.br>
2024-07-18 17:35:15 -0300

    updated data evaluation

--- src/credit_card_fraud_detection/pipelines/data_evaluation/nodes.py
+++ src/credit_card_fraud_detection/pipelines/data_evaluation/nodes.py
@@ -1,38 +1,76 @@
-from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, f1_score
+import pandas as pd
+import mlflow
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-def test_model( X_test, y_test,y_predict):
+def get_new_run_id(metrics_df, model_name):
+    """
+    Generate a new run ID for the given model name.
+
+    Args:
+    metrics_df (DataFrame): DataFrame containing existing metrics.
+    model_name (str): Name of the model.
+
+    Returns:
+    str: New run ID.
+    """
+    if metrics_df.empty or model_name not in metrics_df['model_name'].values:
+        return f"{model_name}_1"
+    else:
+        last_run_id = metrics_df[metrics_df['model_name'] == model_name]['run_id'].max()
+        last_run_num = int(last_run_id.split('_')[-1])
+        return f"{model_name}_{last_run_num + 1}"
+
+def test_model(model_name, y_test, y_predict, metrics_df):
     """
     Test a machine learning model and evaluate its performance.
 
     Args:
-    randomized_search (RandomizedSearchCV): Trained model.
+    model_name (str): Name of the model.
     X_test (DataFrame): Test features.
     y_test (Series): Test labels.
+    y_predict (Series): Predicted labels.
+    metrics_df (DataFrame): DataFrame to store metrics.
 
     Returns:
-    DataFrame: Evaluation metrics.
-    estimator: Best estimator.
+    DataFrame: Updated metrics DataFrame.
     """
-    # Best model
-    result = {}
-
-
-    print("Random Forest Evaluation (CN vs AD) - gridsearch 5 folds")
-    print("------------------------------------------------------")
-    print('Score (Accuracy): ')
-    print(accuracy_score(y_test, y_predict))
-    result['accuracy'] = accuracy_score(y_test, y_predict)
-    print("------------------------------------------------------")
-    print('Precision Score (tp / (tp + fp)):')
-    print(precision_score(y_test, y_predict))
-    result['precision_score'] = precision_score(y_test, y_predict)
-    print("------------------------------------------------------")
-    print('Recall Score (tp / (tp + fn)):')
-    print(recall_score(y_test, y_predict))
-    #result['recall_score'] = recall_score(y_test, y_predict)
-    print("------------------------------------------------------")
-    print('F1 Score (F1 = 2 * (precision * recall) / (precision + recall) ):')
-    print(f1_score(y_test, y_predict))
-    #result['f1_score'] = f1_score(y_test, y_predict)
+    run_id = get_new_run_id(metrics_df, model_name)
     
-    return result
+    accuracy = accuracy_score(y_test, y_predict)
+    precision = precision_score(y_test, y_predict)
+    recall = recall_score(y_test, y_predict)
+    f1 = f1_score(y_test, y_predict)
+    ''' 
+    # Log metrics to MLflow
+    with mlflow.start_run(run_name=run_id, nested=True):
+        mlflow.log_param("model_name", model_name)
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_metric("precision_score", precision)
+        mlflow.log_metric("recall_score", recall)
+        mlflow.log_metric("f1_score", f1)'''
+
+    # Update the metrics DataFrame
+    new_metrics = {
+        'model_name': model_name,
+        'run_id': run_id,
+        'accuracy': accuracy,
+        'precision_score': precision,
+        'recall_score': recall,
+        'f1_score': f1
+    }
+    metrics_df = metrics_df.append(new_metrics, ignore_index=True)
+
+    return metrics_df
+
+def copy_temp_to_final(temp_dataset):
+    """
+    Copy the temporary dataset to the final CSV file.
+
+    Args:
+    temp_dataset (DataFrame): The temporary DataFrame containing metrics.
+    final_dataset_path (str): The path to the final CSV file.
+
+    Returns:
+    None
+    """
+    return temp_dataset
--- src/credit_card_fraud_detection/pipelines/data_evaluation/pipeline.py
+++ src/credit_card_fraud_detection/pipelines/data_evaluation/pipeline.py
@@ -1,15 +1,18 @@
-"""
-This is a boilerplate pipeline 'data_evaluation'
-generated using Kedro 0.19.5
-"""
-
 from kedro.pipeline import Pipeline, pipeline, node
-from .nodes import test_model
+from .nodes import test_model,copy_temp_to_final
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(
-        func= test_model,
-        inputs = ['X_test','y_test','y_predict'],
-        outputs = 'metrics')
+            func=test_model,
+            inputs=["params:isolation_forest.name","y_test","y_predict_IF", "df_metrics"],
+            outputs="temp_df_metrics",
+            name="evaluation_model_IF",
+            tags=["evaluation"]),
+        node(
+            func=copy_temp_to_final,
+            inputs=["temp_df_metrics"],
+            outputs="df_metrics",
+            name="copy_temp_df_to_final",
+            tags=["evaluation"])
     ])
