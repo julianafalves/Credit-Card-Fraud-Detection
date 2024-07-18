64e25b7d4fcdf8659ff50393eb091837ac6a5df2
julianafalves <julianaferreira@estudante.ufscar.br>
2024-07-18 17:37:39 -0300

    add CBLOF to data science modeling

--- src/credit_card_fraud_detection/pipelines/data_science/nodes.py
+++ src/credit_card_fraud_detection/pipelines/data_science/nodes.py
@@ -1,21 +1,54 @@
 from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
+from pyod.models.cblof import CBLOF
+from sklearn.cluster import KMeans
+
 import numpy as np
+import mlflow
 
-def train(X_train,y_train):
-    clf = IsolationForest(
-        random_state=42,
-        bootstrap=True,
-        contamination=0.001,
-        n_estimators=500
-    )
+def train(X_train, y_train, model_params, model_type):
+    # Check if there's an active run
+    if mlflow.active_run():
+        print("Active run found. Ending the run.")
+        mlflow.end_run()
 
-    clf.fit(X_train,y_train)
+    # Initialize the model based on the model_type using **model_params
+    if model_type == 'IF':
+        clf = IsolationForest(**model_params)
+        clf.fit(X_train, y_train)
+
+    elif model_type == 'CBLOF':
+        clf = CBLOF(contamination=model_params['contamination'],
+                    n_clusters=model_params['n_clusters'],
+                    clustering_estimator=KMeans(n_clusters=model_params['n_clusters']))
+        clf.fit(X_train)
+
+
+    
+    """
+    # Register the model with MLflow
+    with mlflow.start_run() as run:
+        mlflow.log_params(model_params)  # log model parameters
+        mlflow.sklearn.log_model(clf, "model")
+        
+        mlflow.register_model(
+            "runs:/{}/model".format(run.info.run_id),
+            "MyModel"
+        )
+
+    # End the run explicitly
+    mlflow.end_run()
+
+    """
     return clf
 
 def predict(X_test, clf):
     pred = clf.predict(X_test)
     pred_mod = np.array(list(map(lambda x: 1*(x == -1), pred)))
     print(pred_mod[0:5])
+    
+    #mlflow.sklearn.log_model(clf, "iForest")
+
 
 
     return pred_mod
--- src/credit_card_fraud_detection/pipelines/data_science/pipeline.py
+++ src/credit_card_fraud_detection/pipelines/data_science/pipeline.py
@@ -4,12 +4,30 @@ from kedro.pipeline import Pipeline, pipeline,node
 from .nodes import train,predict
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([node(func=train,
-                          inputs=[ 'X_train', 'y_train'],
-                          outputs='model'),
-                    node(
-                        func=predict,
-                        inputs=['X_test','model'],
-                        outputs='y_predict',
-                        name="predict",
+    return pipeline([
+            node(
+                func=train,
+                inputs=["X_train", "y_train", "params:isolation_forest.params", "params:isolation_forest.name"],
+                outputs="model_if",
+                name="train_IF_model",
+                tags=["isolation_forest"]
+            ),
+            node(
+                func=train,
+                inputs=["X_train", "y_train", "params:CBLOF.params", "params:CBLOF.name"],
+                outputs="model_CBLOF",
+                name="train_CBLOF_model",
+                tags=["local_outlier_factor"]
+            ),
+            node(
+                func=predict,
+                inputs=['X_test','model_if'],
+                outputs='y_predict_IF',
+                name="predict_IF",
+                    ),
+            node(
+                func=predict,
+                inputs=['X_test','model_CBLOF'],
+                outputs='y_predict_CBLOF',
+                name="predict_CBLOF",
                     )])
